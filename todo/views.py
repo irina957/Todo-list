77bb6906693692a510ac88e13cf0692f91035670
iryna <irakirvas78@gmail.com>
2025-11-13 19:32:29 +0200

    feat: implement class-based views and URL routing for tasks and tags

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from todo.models import Task, Tag
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    queryset = Task.objects.prefetch_related("tags").order_by("done", "-created")
+    paginate_by = 5
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tasks_list")
+    template_name = "todo/task_form.html"
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    template_name = "todo/task_form.html"
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tasks_list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    template_name = "todo/task_confirm_delete.html"
+    success_url = reverse_lazy("todo:tasks_list")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    paginate_by = 5
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tags_list")
+    template_name = "todo/tag_form.html"
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    template_name = "todo/tag_form.html"
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tags_list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    template_name = "todo/tag_confirm_delete.html"
+    success_url = reverse_lazy("todo:tags_list")
+
+
+def task_toggle_done(request, pk):
+    task = Task.objects.get(pk=pk)
+    if task.done:
+        task.done = False
+        task.save()
+    else:
+        task.done = True
+        task.save()
+    return redirect("todo:tasks_list")
